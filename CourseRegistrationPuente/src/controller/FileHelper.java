482a9b1be40d9b79f9aac335e8031c109249fb92
Nicole Puente <93219605+Nicole-Puente@users.noreply.github.com>
2022-04-27 14:25:18 -0500

    Delete CourseRegistrationPuente directory

--- CourseRegistrationPuente/src/controller/FileHelper.java
+++
@@ -1,9 +0,0 @@
-package controller;
-import java.util.ArrayList;
-
-public interface FileHelper {
-	public boolean doesAFileExist();
-	public boolean writeFile(ArrayList<?> readFile);
-	public ArrayList<?> readFile();
-
-}
